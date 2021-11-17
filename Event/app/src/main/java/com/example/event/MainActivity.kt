8bd8fe8c7021f6f7c75526bdae37feb6fc3db83d
annisann <ennoza@student.ub.ac.id>
2021-11-17 18:20:04 +0700

    init project viewmodel-livedata

--- Event/app/src/main/java/com/example/event/MainActivity.kt
+++ Event/app/src/main/java/com/example/event/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private val btn1Name: String = "segitiga"
