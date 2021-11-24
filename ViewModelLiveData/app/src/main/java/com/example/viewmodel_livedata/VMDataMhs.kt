4a22d9ba37dcf7149d290886ddc0e5f2e50e2940
annisann <ennoza@student.ub.ac.id>
2021-11-24 22:40:24 +0700

    update live data

--- ViewModelLiveData/app/src/main/java/com/example/viewmodel_livedata/VMDataMhs.kt
+++ ViewModelLiveData/app/src/main/java/com/example/viewmodel_livedata/VMDataMhs.kt
@@ -1,14 +1,25 @@
 package com.example.viewmodel_livedata
 
-class VMDataMhs {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class VMDataMhs : ViewModel() {
     var mhsList = mutableListOf<DataMhs>()
+    var liveDataMhs = MutableLiveData<List<DataMhs>>()
+
     init {
         mhsList.add(DataMhs("Annisa", "175150200111068", "PSI-B"))
     }
 
-    fun getData() = mhsList
+    fun getData() : LiveData<List<DataMhs>> {
+        liveDataMhs.value = mhsList
+        return liveDataMhs
+    }
 
     fun addData(newData: DataMhs){
         mhsList.add(newData)
+        liveDataMhs.value = mhsList
     }
+
 }
